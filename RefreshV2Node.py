73bea6a3dcfecc8553aeabfed0cd6cbe93a4f31b
Guanming <40348297+GuanmingLu@users.noreply.github.com>
2023-03-28 23:08:00 +0800

    Update RefreshV2Node.py

--- RefreshV2Node.py
+++ RefreshV2Node.py
@@ -1,5 +1,6 @@
 import datetime
 import requests
+import base64
 
 list = [
     "https://clashnode.com/wp-content/uploads/{y}/{m}/{y}{m}{d}.txt",
@@ -8,7 +9,7 @@ list = [
 ]
 
 
-def access_url(url: str, date: datetime.datetime) -> str:
+def access_url(url: str, date: datetime.datetime) -> requests.Response:
 	year = str(date.year)
 	month = str(date.month).zfill(2)
 	day = str(date.day).zfill(2)
@@ -17,19 +18,28 @@ def access_url(url: str, date: datetime.datetime) -> str:
 			formatted_url = url.format(y=year, m=month, d=day, n=str(i))
 			result = requests.get(url=formatted_url)
 			if result.ok:
-				return formatted_url
+				return result
 	else:
 		formatted_url = url.format(y=year, m=month, d=day)
 		result = requests.get(url=formatted_url)
 		if result.ok:
-			return formatted_url
-	return ""
+			return result
+	return None
 
-
+datas = ""
 for url in list:
 	for delta_day in range(1, -10, -1):
 		date = datetime.datetime.now() + datetime.timedelta(days=delta_day)
-		formatted_url = access_url(url, date)
-		if formatted_url != "":
-			print(formatted_url)
+		result = access_url(url, date)
+		if result is not None:
+			print(result.url)
+			# print(result.content)
+			datas += base64.b64decode(result.content).decode("utf-8")
+			if not datas.endswith("\n"):
+				datas += "\n"
 			break
+
+# print(datas)
+
+with open("v2ray.txt", "w") as f:
+	f.write(datas)
