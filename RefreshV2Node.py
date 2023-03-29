554f222958dd40fe77ec7cbc6c0d7d8c951e78a3
Guanming <40348297+GuanmingLu@users.noreply.github.com>
2023-03-29 20:19:08 +0800

    Update RefreshV2Node.py

--- RefreshV2Node.py
+++ RefreshV2Node.py
@@ -3,27 +3,26 @@ import requests
 import base64
 
 
-def date_list(start_date_delta: int = 1, end_date_delta: int = -10, step: int = -1) -> list[datetime.datetime]:
-	return [datetime.datetime.now() + datetime.timedelta(days=i) for i in range(start_date_delta, end_date_delta, step)]
+def date_list(start_date_delta: int = 1, end_date_delta: int = -10, step: int = -1) -> list[(str, str, str)]:
+	dates = [datetime.datetime.now() + datetime.timedelta(days=i) for i in range(start_date_delta, end_date_delta, step)]
+	return [(f"{d.year}", f"{d.month:02d}", f"{d.day:02d}") for d in dates]
 
 
 urls = [
-    [f"https://clashfree.eu.org/wp-content/uploads/rss/{date.year}{date.month:02d}{date.day:02d}.txt" for date in date_list()],
-    [f"https://oneclash.cc/wp-content/uploads/{date.year}/{date.month:02d}/{date.year}{date.month:02d}{date.day:02d}.txt" for date in date_list()],
-    [f"https://clashnode.com/wp-content/uploads/{date.year}/{date.month:02d}/{date.year}{date.month:02d}{date.day:02d}.txt" for date in date_list()],
-    [f"https://v2rayshare.com/wp-content/uploads/{date.year}/{date.month:02d}/{date.year}{date.month:02d}{date.day:02d}.txt" for date in date_list()],
-    [f"https://freenode.me/wp-content/uploads/{date.year}/{date.month:02d}/{date.day:02d}{num}.txt" for date in date_list() for num in range(9, -1, -1)] + [
-        f"https://freenode.me/wp-content/uploads/{date.year}/{date.month:02d}/{date.month:02d}{date.day:02d}{num}.txt" for date in date_list()
-        for num in range(9, -1, -1)
-    ],
+    [f"https://clashfree.eu.org/wp-content/uploads/rss/{y}{m}{d}.txt" for (y, m, d) in date_list()],
+    [f"https://oneclash.cc/wp-content/uploads/{y}/{m}/{y}{m}{d}.txt" for (y, m, d) in date_list()],
+    [f"https://clashnode.com/wp-content/uploads/{y}/{m}/{y}{m}{d}.txt" for (y, m, d) in date_list()],
+    [f"https://v2rayshare.com/wp-content/uploads/{y}/{m}/{y}{m}{d}.txt" for (y, m, d) in date_list()],
+    [f"https://freenode.me/wp-content/uploads/{y}/{m}/{d}{n}.txt" for (y, m, d) in date_list() for n in range(9, -1, -1)] +
+    [f"https://freenode.me/wp-content/uploads/{y}/{m}/{m}{d}{n}.txt" for (y, m, d) in date_list() for n in range(9, -1, -1)],
     ["https://jiang.netlify.com/"],
-    ["https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2"]
+    ["https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2"],
 ]
 
 # print(urls)
 # exit(0)
 
-datas = ""
+datas = []
 for url_list in urls:
 	for url in url_list:
 		result = requests.get(url)
@@ -31,12 +30,12 @@ for url_list in urls:
 			print(result.url)
 			# print(result.content)
 			server_list = base64.b64decode(result.content).decode("utf-8")
-			if not server_list.endswith("\n"):
-				server_list += "\n"
-			datas += server_list
+			for server in server_list.split("\n"):
+				if datas.count(server) == 0:
+					datas.append(server)
 			break
 
 # print(datas)
 
 with open("v2ray.txt", "w") as f:
-	f.write(base64.b64encode(datas.encode("utf-8")).decode("utf-8"))
+	f.write(base64.b64encode("\n".join(datas).encode("utf-8")).decode("utf-8"))
