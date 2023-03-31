94c0b7aea3e47e141e1e5addd25a43c563831052
Guanming <40348297+GuanmingLu@users.noreply.github.com>
2023-04-01 00:45:18 +0800

    Update GetClashConfig.py

--- GetClashConfig.py
+++ GetClashConfig.py
@@ -1,13 +1,17 @@
 import requests
+import argparse
 
-source_url = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"
+parser = argparse.ArgumentParser(description="获取Clash配置文件模板")
+parser.add_argument("-u", "--url", help="配置文件地址", default="https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_MultiMode.ini")
+parser.add_argument("-o", "--output", help="输出文件名", default="clashConfig.ini")
+args = parser.parse_args()
 
 if __name__ == "__main__":
 	try:
-		result = requests.get(source_url)
+		result = requests.get(args.url)
 		if result.ok:
 			config = result.content.decode("utf-8").replace("gstatic", "google")
-			with open("clashConfig.ini", "w", encoding="utf-8") as f:
+			with open(args.output, "w", encoding="utf-8") as f:
 				f.write(config)
 			print("获取配置文件成功")
 		else:
