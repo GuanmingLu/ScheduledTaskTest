c481fd18552f8cbfff688f1a55784b877f5d0a59
Guanming <40348297+GuanmingLu@users.noreply.github.com>
2023-03-31 19:45:02 +0800

    Create V2Nodes2Clash.py

---
+++ V2Nodes2Clash.py
@@ -0,0 +1,30 @@
+from urllib.parse import urlencode
+import requests
+
+urls = [
+    "https://raw.githubusercontent.com/GuanmingLu/ScheduledTaskTest/main/v2ray.txt",
+    "https://raw.githubusercontent.com/GuanmingLu/ScheduledTaskTest/main/acceleratorNodes.txt",
+]
+
+data = {
+    "target": "clash",
+    "url": "|".join(urls),
+    "config": "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_MultiMode.ini",
+}
+
+access_url = "https://sub.xeton.dev/sub?" + urlencode(data)
+
+print(access_url)
+
+if __name__ == "__main__":
+	try:
+		result = requests.get(access_url)
+		if result.ok:
+			# print(result.content.decode("utf-8"))
+			with open("clash.yaml", "w", encoding="utf-8") as f:
+				f.write(result.content.decode("utf-8"))
+			print("获取订阅内容成功")
+		else:
+			print(f"获取订阅内容失败: {result.status_code}")
+	except Exception as e:
+		print(f"获取订阅内容失败: {e}")
