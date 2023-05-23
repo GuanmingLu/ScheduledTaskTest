004db3d755a79f6c68aa9d9dff3a251b5e16eed1
GuanmingLu <40348297+GuanmingLu@users.noreply.github.com>
2023-05-24 00:51:35 +0800

    修改测速设置

--- GetClashConfig.py
+++ GetClashConfig.py
@@ -2,7 +2,10 @@ import requests
 import argparse
 
 parser = argparse.ArgumentParser(description="获取Clash配置文件模板")
-parser.add_argument("-u", "--url", help="配置文件地址", default="https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_MultiMode.ini")
+parser.add_argument("-u",
+                    "--url",
+                    help="配置文件地址",
+                    default="https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_MultiMode.ini")
 parser.add_argument("-o", "--output", help="输出文件名", default="clashConfig.ini")
 args = parser.parse_args()
 
@@ -10,7 +13,7 @@ if __name__ == "__main__":
 	try:
 		result = requests.get(args.url)
 		if result.ok:
-			config = result.content.decode("utf-8").replace("gstatic", "google")
+			config = result.content.decode("utf-8").replace("gstatic", "google").replace("`300,,", "`60,3,")
 			with open(args.output, "w", encoding="utf-8") as f:
 				f.write(config)
 			print("获取配置文件成功")
